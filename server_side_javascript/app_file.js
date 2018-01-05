1aaa1e62d789426fc6f2df3037507f060b6df31f
actmember <actmember@actmembers-iMac.local>
2018-01-05 17:55:48 +0900

    20180105w

--- server_side_javascript/app_file.js
+++ server_side_javascript/app_file.js
@@ -10,18 +10,52 @@ app.set('views', './views_file');
 app.set('view engine', 'pug');
 // 라우팅 -> 위 코드 통해 위치 경로 및 파일 종류 정하고 아래 render로 연결
 app.get('/topic/new', function(req, res){
-  res.render('new');
-})
+  fs.readdir('data',function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error')
+    }
+    res.render('new',{topics:files});
+  });
+});
 // 해당 페이지 바로 호출가능 -> get을 이용하기 떄문
-app.get('/topic', function(req, res){
+app.get(['/topic', '/topic/:id'], function(req, res){
   fs.readdir('data', function(err, files){
     if(err){
       res.status(500).send('Internal Server Error')
     }
-    // render 는 파일템플릿 이름, 주입하고자 하는것 들어감
-    res.render('view', {topics:files});
+    var id = req.params.id;
+    // Id값이 있을때
+    if(id){
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error')
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      })
+    }else{
+      // render 는 파일템플릿 이름, 주입하고자 하는것 들어감
+      res.render('view', {topics:files, title:'Welcom', description:'Hello, JavaScript for server.'});
+    }
   })
 });
+// app.get('/topic/:id', function(req, res){
+//   var id = req.params.id;
+//   fs.readdir('data', function(err, files){
+//     if(err){
+//       console.log(err)
+//       res.status(500).send('Internal Server Error')
+//     }
+//     fs.readFile('data/'+id, 'utf8', function(err, data){
+//       if(err){
+//         console.log(err);
+//         res.status(500).send('Internal Server Error')
+//       }
+//       res.render('view', {topics:files, title:id, description:data});
+//     })
+//   })
+// })
 app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
@@ -30,7 +64,7 @@ app.post('/topic', function(req, res){
         console.log(err);
         res.status(500).send('Internal Server Error');
     }
-    res.send('Success!');
+    res.redirect('/topic/'+title);
   })
 })
 app.listen(3000, function(){
